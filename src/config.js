61e7dac2a46a288793d349e4b10a5fa6ac1983e8
coolkla2002 <coolkla2002@gmail.com>
2026-01-13 19:53:42 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
 // ✅ ตอนทดสอบในเครื่อง (Localhost) ให้เปิดบรรทัดนี้
-const API_URL = "http://localhost:3001";
+const API_URL = "https://hotel-booking-web-kfks.onrender.com";
 
 // 🚀 ตอนเอาขึ้นเว็บจริง (Deploy) ให้สลับมาเปิดบรรทัดนี้แทน (แล้วปิดบรรทัดบน)
 // const API_URL = "https://ชื่อโปรเจคของคุณ.onrender.com";
