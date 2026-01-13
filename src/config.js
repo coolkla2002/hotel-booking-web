503d1f60e41ec4af65e9d815fc6e6226e3626831
coolkla2002 <coolkla2002@gmail.com>
2026-01-13 19:56:14 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,4 @@
-
-
-// ✅ ตอนทดสอบในเครื่อง (Localhost) ให้เปิดบรรทัดนี้
+// แก้ไขให้เป็น URL ของ Render ที่คุณใช้งานอยู่
 const API_URL = "https://hotel-booking-web-kfks.onrender.com";
-
-// 🚀 ตอนเอาขึ้นเว็บจริง (Deploy) ให้สลับมาเปิดบรรทัดนี้แทน (แล้วปิดบรรทัดบน)
-// const API_URL = "https://ชื่อโปรเจคของคุณ.onrender.com";
 
 export default API_URL;
