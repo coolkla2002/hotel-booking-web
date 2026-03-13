555c0276f0df69022f75f7cae987cd83cc2eb334
coolkla2002 <coolkla2002@gmail.com>
2026-03-13 15:56:19 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // แก้ไขให้เป็น URL ของ Render ที่คุณใช้งานอยู่
-const API_URL = "https://hotel-booking-web-kfks.onrender.com";
+const API_URL = "https://hotel-booking-web-eight.vercel.app";
 
 export default API_URL;
